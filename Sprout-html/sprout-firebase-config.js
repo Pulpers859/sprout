296a9105c863d2d5c3df0ad157260d7df6bf10bf
Pulpers859 <145473723+Pulpers859@users.noreply.github.com>
2026-05-20 13:10:42 -0400

    Remove committed Firebase config and add secret guardrails

--- Sprout-html/sprout-firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.SPROUT_FIREBASE = {
-  apiKey: "AIzaSyBQId5uNF19Fh3_Aw4srXrK6XhWACT_Y3w",
-  authDomain: "sprout-budgeting.firebaseapp.com",
-  projectId: "sprout-budgeting",
-  storageBucket: "sprout-budgeting.firebasestorage.app",
-  messagingSenderId: "1030182595600",
-  appId: "1:1030182595600:web:7a113eb2ec1a9dade8ae95"
-};
